7f59903ca9decdbdf436c6ee71ce2fbb9e1cb64e
amespinosa <aespinosa@puntosleal.com>
2020-01-26 19:13:23 -0500

    Nuevo puerto y ip

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.use("/", indexRoutes);
 app.use("/events", eventsRoutes);
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, '127.0.0.1' , function () {
+  console.log('Example app listening on port 8080!');
 });
